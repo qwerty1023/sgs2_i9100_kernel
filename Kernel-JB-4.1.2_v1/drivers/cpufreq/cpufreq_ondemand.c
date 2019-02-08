8f44caab420415ca7974fe9f1f30169e9fc6407d
Sandra <sandra_oth@ngs.ru>
2019-02-08 15:47:22 +0200

    ondemand gov tune

--- Kernel-JB-4.1.2_v1/drivers/cpufreq/cpufreq_ondemand.c
+++ Kernel-JB-4.1.2_v1/drivers/cpufreq/cpufreq_ondemand.c
@@ -61,7 +61,7 @@
 static unsigned int min_sampling_rate;
 
 #define LATENCY_MULTIPLIER			(1000)
-#define MIN_LATENCY_MULTIPLIER			(100)
+#define MIN_LATENCY_MULTIPLIER			(20)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
 
 static void do_dbs_timer(struct work_struct *work);
@@ -887,10 +887,10 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		mutex_lock(&dbs_mutex);
 		mutex_destroy(&this_dbs_info->timer_mutex);
 		dbs_enable--;
-		mutex_unlock(&dbs_mutex);
 		if (!dbs_enable)
 			sysfs_remove_group(cpufreq_global_kobject,
 					   &dbs_attr_group);
+		mutex_unlock(&dbs_mutex);
 
 		break;
 
